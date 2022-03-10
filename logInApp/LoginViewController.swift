1ace0c0a3685f33b28f630e7631fa9dbc622b650
iReek <i_r_e_k@mail.ru>
2022-03-10 23:07:10 +0300

    HW3poPrepodu

---
+++ logInApp/LoginViewController.swift
@@ -0,0 +1,158 @@
+//
+//  ViewController.swift
+//  logInApp
+//
+//  Created by Reek i on 06.03.2022.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    // MARK: IB Outlets
+    @IBOutlet var userNameTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        passwordTextField.returnKeyType = .done
+    }
+    
+    // MARK: Private properties
+    private let user = "Irek"
+    private let password = "12345"
+    
+    // MARK Navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let welcomeVC = segue.destination as! WelcomeViewController
+        welcomeVC.user = user
+    }
+    
+    // MARK: IBActions
+    @IBAction func logInPressed() {
+        if userNameTextField.text != user || passwordTextField.text != password {
+            showAlert(title: "Invalid login or password",
+                      message: "Please, enter correct login and password",
+                      textField: passwordTextField
+            )
+            return
+        }
+        
+        performSegue(withIdentifier: "showWelcomeVC", sender: nil)
+    }
+    
+    @IBAction func forgotUserNamePressed() {
+        showAlert(title: "Oops!",
+                  message: "Your name is \(user)")
+    }
+    
+    @IBAction func forgotPasswordPressed() {
+        showAlert(title: "Oops!",
+                  message: "Your password is \(password)")
+    }
+    
+    @IBAction func unwindSegue(segue: UIStoryboardSegue) {
+        userNameTextField.text = nil
+        passwordTextField.text = nil
+    }
+    
+}
+
+// MARK: Alert Controller
+extension LoginViewController {
+    private func showAlert(title: String, message: String, textField: UITextField? = nil) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            textField?.text = nil
+        }
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
+// MARK: Work with keyboard
+extension LoginViewController: UITextFieldDelegate {
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super .touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == userNameTextField {
+            passwordTextField.becomeFirstResponder()
+        } else {
+            logInPressed()
+        }
+        return true
+    }
+}
+
+
+//import UIKit
+//
+//class ViewController: UIViewController, UITextFieldDelegate {
+//
+//    @IBOutlet var userNameTextField: UITextField!
+//    @IBOutlet var passwordTextField: UITextField!
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        userNameTextField.delegate = self
+//        // userNameTextField.tag = 0
+//        userNameTextField.returnKeyType = .next
+//        passwordTextField.delegate = self
+//        // passwordTextField.tag = 1
+//        passwordTextField.returnKeyType = .done
+//
+//    }
+//
+//    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+//        if let _ = touches.first {
+//            view .endEditing(true)
+//            super.touchesBegan(touches, with: event)
+//        }
+//    }
+//
+//    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+//        if userNameTextField.text == "Irek" && passwordTextField.text == "12345" {
+//            guard let welcomeUserVC = segue.destination as? WelcomeViewController else  { return }
+//            welcomeUserVC.welcomeUserName = userNameTextField.text
+//
+//        } else {
+//            showAlert(title: "Алйо!", subtitle: "User name or password is wrong!")
+//        }
+//    }
+//
+//    @IBAction func forgotUserNameButtonPressed() {
+//        showAlert(title: "Yo!", subtitle: "Your name is Irek")
+//    }
+//
+//    @IBAction func forgotPasswordButtonPresseed() {
+//        showAlert(title: "Yo!", subtitle: "Password is 12345")
+//    }
+//
+//    @IBAction func unwind(segue: UIStoryboardSegue) {
+//        userNameTextField.text = ""
+//        passwordTextField.text = ""
+//    }
+//
+//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+//        if userNameTextField.isFirstResponder {
+//            passwordTextField.becomeFirstResponder()
+//        }
+//        return true
+//    }
+//
+//    func showAlert(title: String, subtitle: String? = nil) {
+//        let alertController = UIAlertController(title: title,
+//                                                message: subtitle,
+//                                                preferredStyle: .alert)
+//        let done = UIAlertAction(title: "Понято", style: .default)
+//        alertController.addAction(done)
+//
+//        present(alertController, animated: true)
+//    }
+//}
+//
--- logInApp/WelcomeViewController.swift
+++ logInApp/WelcomeViewController.swift
@@ -8,19 +8,71 @@
 import UIKit
 
 class WelcomeViewController: UIViewController {
-
-    @IBOutlet var welcomeUserNameLabel: UILabel!
     
-    var welcomeUserName: String!
+    // MARK: IB Outlets
+    @IBOutlet var welcomeLabel: UILabel!
+    
+    // MARK: Public properties
+    var user = ""
+    
+    // MARK: Private properties
+    private let primaryColor = UIColor(
+        red: 210/255,
+        green: 109/255,
+        blue: 128/255,
+        alpha: 1
+        )
+    private let secondaryColor = UIColor(
+        red: 107/255,
+        green: 148/255,
+        blue: 230/255,
+        alpha: 1
+    )
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        welcomeUserNameLabel.text = "Welcome, \(welcomeUserName ?? "")!"
+        view.addVerticalGradientLayer(topColor: primaryColor, bottomColor: secondaryColor)
+        welcomeLabel.text = "Welcome, \(user)!"
     }
     
-    @IBAction func logOutButtonPressed() {
-        dismiss(animated: true)
-    }
+    // MARK: IB Actions
+        @IBAction func logOutButtonPressed() {
+            dismiss(animated: true)
+        }
     
 }
+
+// MARK: Set background color
+extension UIView {
+    func addVerticalGradientLayer(topColor: UIColor, bottomColor: UIColor) {
+        let gradient = CAGradientLayer()
+        gradient.frame = bounds
+        gradient.colors = [topColor.cgColor, bottomColor.cgColor]
+        gradient.locations = [0.0, 1.0]
+        gradient.startPoint = CGPoint(x: 0, y: 0)
+        gradient.endPoint = CGPoint(x: 0, y: 1)
+        layer.insertSublayer(gradient, at: 0)
+    }
+}
+
+
+
+//import UIKit
+//
+//class WelcomeViewController: UIViewController {
+//
+//    @IBOutlet var welcomeUserNameLabel: UILabel!
+//
+//    var welcomeUserName: String!
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        welcomeUserNameLabel.text = "Welcome, \(welcomeUserName ?? "")!"
+//    }
+//
+//    @IBAction func logOutButtonPressed() {
+//        dismiss(animated: true)
+//    }
+//
+//}
